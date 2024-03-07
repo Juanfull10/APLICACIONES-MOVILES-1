98cadac6c57d15cdb0c3d6bae5c9eebaa838a95f
Juan <perazajuan770@gmail.com>
2024-03-07 09:52:19 -0500

    actualizado mejoras

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -28,40 +28,135 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void suma(View view){
-        int suma=Integer.parseInt(valor1.getText().toString())+Integer.parseInt(valor2.getText().toString());
-        resultado.setText(suma+"");
+        String numero = valor1.getText().toString();
+        String numero2 = valor2.getText().toString();
+
+        if (numero.isEmpty() && numero2.isEmpty() ) {
+            resultado.setText("Digite Numeros");
+        }else if(numero.isEmpty() && !numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el primer espacio");
+        }else if(!numero.isEmpty() && numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el segundo espacio");
+        }else if(!numero.isEmpty() && !numero2.isEmpty() ){
+            double num1 = Double.parseDouble(valor1.getText().toString());
+            double num2 = Double.parseDouble(valor2.getText().toString());
+
+            Double suma= num1+num2;
+            resultado.setText(suma+"");
+        }
     }
 
     public void resta(View view){
-        int resta= Integer.parseInt(valor1.getText().toString())- Integer.parseInt(valor2.getText().toString());
-        resultado.setText(resta+"");
+        String numero = valor1.getText().toString();
+        String numero2 = valor2.getText().toString();
 
+        if (numero.isEmpty() && numero2.isEmpty() ) {
+            resultado.setText("Digite Numeros");
+        }else if(numero.isEmpty() && !numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el primer espacio");
+        }else if(!numero.isEmpty() && numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el segundo espacio");
+        }else if(!numero.isEmpty() && !numero2.isEmpty() ) {
+            double num1 = Double.parseDouble(valor1.getText().toString());
+            double num2 = Double.parseDouble(valor2.getText().toString());
+
+            Double resta = num1 - num2;
+            resultado.setText(resta+"");
+        }
     }
 
     public void multiplicar(View view){
-        int multiplicacion=Integer.parseInt(valor1.getText().toString())*Integer.parseInt(valor2.getText().toString());
-        resultado.setText(multiplicacion+"");
+        String numero = valor1.getText().toString();
+        String numero2 = valor2.getText().toString();
+
+        if (numero.isEmpty() && numero2.isEmpty() ) {
+            resultado.setText("Digite Numeros");
+        }else if(numero.isEmpty() && !numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el primer espacio");
+        }else if(!numero.isEmpty() && numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el segundo espacio");
+        }else if(!numero.isEmpty() && !numero2.isEmpty() ){
+            double num1 = Double.parseDouble(valor1.getText().toString());
+            double num2 = Double.parseDouble(valor2.getText().toString());
+
+            Double multiplicar= num1*num2;
+            resultado.setText(multiplicar+"");
+        }
     }
 
     public void division(View view){
-        int divi=Integer.parseInt(valor1.getText().toString())/Integer.parseInt(valor2.getText().toString());
-        resultado.setText(divi+"");
-    }
+        String numero = valor1.getText().toString();
+        String numero2 = valor2.getText().toString();
 
-    public static int fibonacci(int numero1) {
-        if(numero1<=1){
-            return numero1;
-        } else {
-            return fibonacci(numero1-1) + fibonacci(numero1-2);
+        if (numero.isEmpty() && numero2.isEmpty() ) {
+            resultado.setText("Digite Numeros");
+        }else if(numero.isEmpty() && !numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el primer espacio");
+        }else if(!numero.isEmpty() && numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el segundo espacio");
+        }else if(!numero.isEmpty() && !numero2.isEmpty() ){
+            double num1 = Double.parseDouble(valor1.getText().toString());
+            double num2 = Double.parseDouble(valor2.getText().toString());
+
+            Double divi= num1/num2;
+            resultado.setText(divi+"");
         }
     }
 
     public void fibo(View view){
-        int numero = Integer.parseInt(valor1.getText().toString());
-        int resultadoFibonacci = fibonacci(numero);
-        resultado.setText(Integer.toString(resultadoFibonacci));
+
+        String numero = valor1.getText().toString();
+        String numero2 = valor2.getText().toString();
+        int num;
+
+        if (numero.isEmpty() && numero2.isEmpty() ){
+            resultado.setText("Digite Numeros");
+        }else if(!numero.isEmpty() && !numero2.isEmpty() ){
+            resultado.setText("Digite en un solo Espacio");
+        }else if(!numero.isEmpty() && numero2.isEmpty()){
+
+            num = Integer.parseInt(valor1.getText().toString());
+            int resultadoFibonacci = fibonacci(num);
+            resultado.setText(Integer.toString(resultadoFibonacci));
+
+        }else if(numero.isEmpty() && !numero2.isEmpty()){
+
+            num = Integer.parseInt(valor2.getText().toString());
+            int resultadoFibonacci = fibonacci(num);
+            resultado.setText(Integer.toString(resultadoFibonacci));
+        }
+    }
+    public static int fibonacci(int numero1) {
+        if(numero1<=1){
+            return numero1;
+        } else{
+            return fibonacci(numero1-1) + fibonacci(numero1-2);
+
+        }
     }
 
+    public void fact(View view){
+        String numero = valor1.getText().toString();
+        String numero2 = valor2.getText().toString();
+        int num;
+
+        if (numero.isEmpty() && numero2.isEmpty() ){
+            resultado.setText("Digite Numeros");
+        }else if(!numero.isEmpty() && !numero2.isEmpty() ){
+            resultado.setText("Digite en un solo Espacio");
+        }else if(!numero.isEmpty() && numero2.isEmpty()){
+
+            num = Integer.parseInt(valor1.getText().toString());
+            int resultadofactorial = factorial(num);
+            resultado.setText(Integer.toString(resultadofactorial));
+
+        }else if(numero.isEmpty() && !numero2.isEmpty()){
+
+            num = Integer.parseInt(valor2.getText().toString());
+            int resultadofactorial = factorial(num);
+            resultado.setText(Integer.toString(resultadofactorial));
+        }
+    }
     public static int factorial(int numero1) {
         int resultado = 0;
 
@@ -72,49 +167,49 @@ public class MainActivity extends AppCompatActivity {
         }
         return resultado;
     }
-    public void fact(View view){
-        int numero = Integer.parseInt(valor1.getText().toString());
-        int resultadofactorial = factorial(numero);
-        resultado.setText(Integer.toString(resultadofactorial));
-    }
 
-    public void poten(View view){
-        int numero = Integer.parseInt(valor1.getText().toString());
-        int numero2 = Integer.parseInt(valor2.getText().toString());
 
-        int resultadoPotencia = poten(numero, numero2, 0, 0);
-        resultado.setText(Integer.toString(resultadoPotencia));
-    }
+    public void Potencia(View view) {
+        String numero = valor1.getText().toString();
+        String numero2 = valor2.getText().toString();
 
-    public static int poten(int base, int exponente, int resultado, int contador){
-        if (contador == exponente){
-            return resultado;
+        if (numero.isEmpty() && numero2.isEmpty() ) {
+            resultado.setText("Digite Numeros");
+        }else if(numero.isEmpty() && !numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el primer espacio");
+        }else if(!numero.isEmpty() && numero2.isEmpty()){
+            resultado.setText("Por Favor dijite un numero en el segundo espacio");
+        }else if(!numero.isEmpty() && !numero2.isEmpty() ){
+
+            int base = Integer.parseInt(valor1.getText().toString());
+            int exponente = Integer.parseInt(valor2.getText().toString());
+            int resultadop = Potencia1(base, exponente);
+
+            resultado.setText(Integer.toString(resultadop));
         }
 
-        int nuevoResultado = multiplicar1(resultado, base, 0, 0);
-        int nuevoContador = contador + 1;
-
-        return poten(base, exponente, nuevoResultado, nuevoContador);
     }
 
-
-    public static int multiplicar1(int numeroASumar,int cantidadVeces, int contador,int resultado){
-
-        if (contador==cantidadVeces){
-            return resultado;
+    public static int Potencia1(int base, int exponente) {
+        if (exponente == 0) {
+            return 1; // Cualquier número elevado a 0 es 1
+        } else if (exponente == 1) {
+            return base; // La potencia de cualquier número a la 1 es el mismo número
+        } else if (exponente > 0) {
+            return PotenciaSumar(base , Potencia1(base, exponente - 1)); // Recursión para calcular la potencia
+        } else {
+            return 1 / (PotenciaSumar(base, Potencia1(base, -exponente - 1))); // Manejo de exponentes negativos
         }
-
-        int nuevoContador=contador+1;
-        int nuevoResultado=resultado;
-        return multiplicar1(numeroASumar,cantidadVeces,nuevoContador,nuevoResultado);
-
     }
-
-
-
-
-
-
+    public static int PotenciaSumar(int num1, int contador) {
+        if (contador == 0) {
+            return 0;
+        } else if (contador > 0) {
+            return num1 + PotenciaSumar(num1, contador - 1);
+        } else {
+            return -PotenciaSumar(num1, -contador);
+        }
+    }
 }
 
 
